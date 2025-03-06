cfea5669df42215164845c0f9f52b692c8e2d163
Iryna Lutska <iryna.lutska@uinno.io>
2025-03-06 20:18:58 +0200

    cleaned code

--- src/modules/rental-data/rental-data.module.ts
+++ src/modules/rental-data/rental-data.module.ts
@@ -1,28 +1,14 @@
 import { Module } from '@nestjs/common';
 import { RentalDataController } from './controllers/rental-data.controller';
 import { RentalDataService } from './services/rental-data.service';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RentalData } from './entity/rental-data.entity';
 import { RentalCarsService } from '../rental-cars/services/rental-cars.service';
 import { RentalCar } from '../rental-cars/entities/rental-car.entity';
 
 @Module({
-  imports: [
-      JwtModule.registerAsync({
-        imports: [ConfigModule],
-        inject: [ConfigService],
-        useFactory: (configService: ConfigService) => ({
-          secret: configService.get<string>('JWT_SECRET'),
-          signOptions: { expiresIn: configService.get<string>('EXPIRES_IN')},
-        }),
-      }),
-      TypeOrmModule.forFeature([RentalData, RentalCar])
-    ],
+  imports: [TypeOrmModule.forFeature([RentalData, RentalCar])],
   controllers: [RentalDataController],
-  providers: [RentalDataService, RentalCarsService]
+  providers: [RentalDataService, RentalCarsService],
 })
-export class RentalDataModule {
-
-}
+export class RentalDataModule {}
